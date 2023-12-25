ff688efbeb5d919bb65df26e295a11fb1d5a572d
anujalatpate <154818835+anujalatpate@users.noreply.github.com>
2023-12-25 22:33:49 +0530

    Angular Assignment4-que2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MarvellousFirst' title`, () => {
+  it(`should have the 'MarvellousFirstApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MarvellousFirst');
+    expect(app.title).toEqual('MarvellousFirstApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MarvellousFirst');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MarvellousFirstApp');
   });
 });
